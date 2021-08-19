2a855f7672771c8295754a8c12e9c9bbce1ab644
techman104 <87010449+techman104@users.noreply.github.com>
2021-08-19 13:34:34 +0900

    viewの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,16 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
    end
+  
+   def new
+    @Item = Item.new
+  end 
+  
+  def create
+    Tweet.create(tweet_params)
+  end
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
 end

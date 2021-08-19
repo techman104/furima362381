8b0d69329bdf4705fd58a8ffcda0a69b2f76c1ee
techman104 <87010449+techman104@users.noreply.github.com>
2021-08-19 13:06:38 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }  
   # validates :encrypted_password,:password,:password_confirmation,length:{minimum:7},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
   validates :password, confirmation: true
-
+  validates :nickname, :family_name, presence: true
+  validates :birth_year, presence: true
      
 end

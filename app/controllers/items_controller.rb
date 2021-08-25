192d5ca3aeb8e5c91a0b7599dcf5908f8d801f37
techman104 <87010449+techman104@users.noreply.github.com>
2021-08-25 11:10:48 +0900

    コードレビューの訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController 
-  before_action :move_to_index, except: [:index, :show]  
+  # before_action :move_to_index, except: [:index, :show]  
   def index
-    @items = Item.all
+    # @items = Item.all
   end
   
   # private

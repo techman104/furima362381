8dd0f41427d6ce8d296346071f82a110c424d1fe
techman104 <87010449+techman104@users.noreply.github.com>
2021-08-25 17:31:58 +0900

    モデルとコントローラーの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,11 @@
 class ItemsController < ApplicationController 
   # before_action :move_to_index, except: [:index, :show]  
   def index
-    # @items = Item.all
+    @items = Item.all
+  end
+  def new
+    @item = Item.new
+    
   end
   
   # private

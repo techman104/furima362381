4e48cdf65d4046aa8ec1cda229122f8eef5f9d53
techman104 <87010449+techman104@users.noreply.github.com>
2021-09-05 17:31:19 +0900

    itemのテスト

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,64 @@
+# テストする名目を書く
+require 'rails_helper'
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+    describe '商品出品' do
+      context '全ての項目に正しく記述されていれば登録できる' do
+        it '全ての項目に正しく記述されていれば登録できる' do
+          expect(@item).to be_valid
+        end  
+      end
+      context '全ての項目に正しく記述されていなければ登録できない' do
+      
+      it 'textが空では登録できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'titleが空では登録できない' do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceの値が300~9999999までの整数でないと登録できない' do
+        @item.price = '10'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid")
+      end
+      it 'categoryが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+       
+      end
+      it 'deliveryが空では登録できない' do
+        @item.delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery can't be blank")
+      end
+      it "areaが空では登録できない" do
+        @item.area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it "statusが空では登録できない" do
+        @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it "dayが空では登録できない" do
+        @item.day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+    end  
+  end
+end

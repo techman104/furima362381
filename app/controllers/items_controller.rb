bcd298dddaf54b83f431878ff9633aa79af1fef7
techman104 <87010449+techman104@users.noreply.github.com>
2021-09-07 20:46:39 +0900

    ログアウト状態からに制限の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController 
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   # before_action :move_to_index, only: [:edit, :update]
   def index
     @items = Item.order("created_at DESC")

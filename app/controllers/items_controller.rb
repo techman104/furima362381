ce136558233ed5cb3bece1fac557742331158557
techman104 <87010449+techman104@users.noreply.github.com>
2021-09-07 19:33:32 +0900

    編集機能の追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,15 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+  def update
+    @item = Item.find(params[:id])
+   
+    if  @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
 
   private

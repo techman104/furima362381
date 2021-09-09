3aca5411a7f4cc5f55f2bf5a8eccf1d2a0901b90
techman104 <87010449+techman104@users.noreply.github.com>
2021-09-09 17:22:47 +0900

    環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,7 +29,7 @@ class OrdersController < ApplicationController
     params.require(:form).permit(:zip_code, :area_id, :city, :address1, :address2, :telephone).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
   end
   def pay_item
-    Payjp.api_key = "sk_test_1f123e9ff90b037af5f44780"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
         card: order_params[:token],    # カードトークン
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_bac9dbd89cd26ab1da256b13"); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
